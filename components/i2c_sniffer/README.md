4f25e54b5c17aeccd8bac59b72200789151b9ded
DanielV <daniel@vikstrom.name>
2024-10-01 15:56:25 +0200

    Add base for I2C Sniffer

---
+++ components/i2c_sniffer/README.md
@@ -0,0 +1,7 @@
+# I2C Sniffer
+A component to sniff I2C data from connacted bus
+
+A fusion of the following code-bases
+
+* [ESP Home Strema Server](https://github.com/oxan/esphome-stream-server) by @oxan
+* [I2C Sniffer](https://github.com/WhitehawkTailor/I2C-sniffer) by @WhitehawkTailor
---
+++ components/i2c_sniffer/binary_sensor.py
@@ -0,0 +1,27 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import binary_sensor
+from esphome.const import (
+    DEVICE_CLASS_CONNECTIVITY,
+    ENTITY_CATEGORY_DIAGNOSTIC,
+)
+from . import ns, I2CSnifferComponent, CONF_I2C_SNIFFER
+
+CONF_CONNECTED = "connected"
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_I2C_SNIFFER): cv.use_id(I2CSnifferComponent),
+        cv.Required(CONF_CONNECTED): binary_sensor.binary_sensor_schema(
+            device_class=DEVICE_CLASS_CONNECTIVITY,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        ),
+    }
+)
+
+
+async def to_code(config):
+    server = await cg.get_variable(config[CONF_I2C_SNIFFER])
+
+    sens = await binary_sensor.new_binary_sensor(config[CONF_CONNECTED])
+    cg.add(server.set_connected_sensor(sens))
